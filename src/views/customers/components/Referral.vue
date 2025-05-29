cef9093da1303a493af9ebfd867fad45ecf63949
Olalekan <lekass205@gmail.com>
2025-05-29 01:05:14 +0100

    fix issue with types

--- src/views/customers/components/Referral.vue
+++ src/views/customers/components/Referral.vue
@@ -3,31 +3,16 @@
         <v-data-table 
             hide-default-footer 
             :items="items" 
-            :loading="loading" 
             :headers="headers"
-            :items-per-page="pagination?.currentPageTotal"
             class="custom-table"
             loading-text="Loading... Please wait" 
         >
-
-            <template v-slot:item.status="{ item }">
-                <app-chip :status="item.status">
-                </app-chip>
-            </template>
-            <template  v-slot:bottom>
-                <TableFooter v-bind="pagination" @next="emits('next', {page: $event})" />
-            </template>
         </v-data-table>
     </section>
 </template>
 
 <script lang="ts" setup>
-import { storeToRefs } from "pinia";
-import { computed, ref } from "vue";
-import { ZoomIn } from 'lucide-vue-next'
-
-import AppChip from "@/components/AppChip.vue";
-import TableFooter from '@/components/AppTableFooter.vue';
+import { ref } from "vue";
 
 
 const props = defineProps<{
